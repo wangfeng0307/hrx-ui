e9309d30bd74cf990d9f28d12aae5f8220d5eb1f
wanghaibo <wanghaibo274@gmail.com>
2020-01-08 14:55:29 +0800

    scss to less - append

--- build/gen-style.js
+++ build/gen-style.js
@@ -1,24 +1,24 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
-const sass = require('gulp-sass');
+const less = require('gulp-less');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const components = require('./components.json')
 
 function buildCss(cb) {
-  gulp.src('../src/styles/index.scss')
-    .pipe(sass())
+  gulp.src('../src/styles/index.less')
+    .pipe(less())
     .pipe(autoprefixer())
     .pipe(cleanCSS())
-    .pipe(rename('lime-ui.css'))
+    .pipe(rename('hrx-ui.css'))
     .pipe(gulp.dest('../lib/styles'));
   cb()
 }
 
 function buildSeperateCss(cb) {
   Object.keys(components).forEach(compName => {
-    gulp.src(`../src/styles/${compName}.scss`)
-      .pipe(sass())
+    gulp.src(`../src/styles/${compName}.less`)
+      .pipe(less())
       .pipe(autoprefixer())
       .pipe(cleanCSS())
       .pipe(rename(`${compName}.css`))
